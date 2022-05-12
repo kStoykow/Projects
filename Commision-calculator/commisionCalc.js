d53fa2b15685911103fe9c8aca998fdc87162749
kStoykow <kristiqn.stoikow11@gmail.com>
2022-05-12 18:09:10 +0300

    Bugfixes in result parser.

--- Commision-calculator/commisionCalc.js
+++ Commision-calculator/commisionCalc.js
@@ -1,7 +1,27 @@
-// const getDealPrice = require('./getRandomDeal');
 function solve() {
+    let btn = document.getElementById('btn');
+    let ul = document.getElementById('output');
+    let balance = document.getElementById('balance');
+
+    function getDealPrice(min, max) {
+        return (Math.random() * (max - min) + min) / 100;
+    }
+    function resultParser(arr, total, ul) {
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+
+        arr.map(e => {
+            let li = document.createElement('li');
+            li.textContent = `${e[0]} -> ${(e[1] / total * 100).toFixed(2)}%`;
+            ul.appendChild(li);
+        });
+    }
+
     function clickHandler(e) {
         e.preventDefault();
+        const min = Number(balance.value) * 100 * 0.1;
+        const max = (Number(balance.value) * 100 * 0.5) + 1;
         let cents = {
             '0.01': 0,
             '0.02': 0,
@@ -20,39 +40,49 @@ function solve() {
             '0.15': 0,
             '0.16': 0,
             '0.17': 0,
+            '0.18': 0,
+            '0.19': 0,
+            '0.20': 0,
+            '0.21': 0,
+            '0.22': 0,
+            '0.23': 0,
+            '0.24': 0,
+            '0.25': 0,
+            '0.26': 0,
+            '0.27': 0,
+            '0.28': 0,
+            '0.29': 0,
+            '0.30': 0,
+            '0.31': 0,
+            '0.32': 0,
+            '0.33': 0,
+            '0.34': 0,
+            '0.35': 0,
+            '0.36': 0,
+            '0.37': 0,
+            '0.38': 0,
+            '0.39': 0,
+            '0.40': 0,
         }
-
-        let balance = document.getElementById('balance');
-        const min = Number(balance.value) * 100 * 0.1;
-        const max = (Number(balance.value) * 100 * 0.5) + 1;
-
-        function getDealPrice(min, max) {
-            return (Math.random() * (max - min) + min) / 100;
-        }
-
 
         for (let i = 0; i < 1000000; i++) {
             let deal = (getDealPrice(min, max) * 0.0026).toFixed(2);
             if (deal < 0.01) {
-                deal = 0.01
+                deal = 0.01;
             }
             cents[deal]++;
+        }
 
-        }
-        function parse(arr, total) {
-            let ul = document.getElementById('output');
-            arr.map(e => {
-                let li = document.createElement('li');
-                li.textContent = `${e[0]} -> ${(e[1] / total * 100).toFixed(2)}%`;
-                ul.appendChild(li);
-            });
-        }
-        
         let total = Object.values(cents).reduce((a, b) => a + b);
-        parse(Object.entries(cents).filter(e => e[1] != 0), total)
+
+        if (balance.value != '') {
+            resultParser(Object.entries(cents).filter(e => e[1] != 0), total, ul);
+        }
+
+        balance.value = '';
     }
-    let btn = document.getElementById('btn');
-    btn.addEventListener('click', clickHandler)
+
+    btn.addEventListener('click', clickHandler);
 }
 
-document.addEventListener('DOMContentLoaded', solve)
+document.addEventListener('DOMContentLoaded', solve);
--- Commision-calculator/getRandomDeal.js
+++ Commision-calculator/getRandomDeal.js
@@ -1,5 +1,4 @@
 function randomNumInRange(min, max) {
     return (Math.random() * (max - min) + min) / 100;
 }
-
 module.exports = randomNumInRange;

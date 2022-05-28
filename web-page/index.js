66acef9ce5e20cdc51eb6a49621abe3b078b152a
kStoykow <kristiqn.stoikow11@gmail.com>
2022-05-28 22:36:22 +0300

    Adding trainers section.

--- web-page/index.js
+++ web-page/index.js
@@ -1,6 +1,5 @@
 function main() {
     const myButton = document.getElementById('go-to-top');
-
     const scrollWindow = function () {
         if (window.scrollY != 0) {
             setTimeout(function () {
@@ -9,7 +8,17 @@ function main() {
             }, 10);
         }
     }
-
     myButton.addEventListener("click", scrollWindow);
+
+    const hiddenElems = document.querySelectorAll('.trainers>div');
+    Array.from(hiddenElems).map(elem => {
+        elem.addEventListener('mouseenter', function show(e) {
+            e.target.firstElementChild.firstElementChild.classList.remove('hidden');
+        });
+        elem.addEventListener('mouseleave', function show(e) {
+            e.target.firstElementChild.firstElementChild.classList.add('hidden');
+        });
+    })
+
 }
 document.addEventListener('DOMContentLoaded', main);

057600f85f88082c11bb3a63dcfa34332fc2e2e0
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-07-26 19:35:48 +0300

    Making todo adding form controlled and usable.

--- todo-list/client/src/App.js
+++ todo-list/client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { AddForm } from './components/AddForm';
 
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -8,17 +9,38 @@ import { Table } from "./components/Table";
 function App() {
 
 	const [todos, setTodos] = useState([]);
+	const [isVisible, setIsVisible] = useState(false);
 
 	useEffect(() => {
 		fetch('http://localhost:3030/jsonstore/todos')
 			.then(res => res.json())
 			.then(data => Object.values(data))
-			.then(todos => setTodos(todos));
+			.then(todos => setTodos(todos))
+			.catch(err => {
+				console.log('err ' + err.message);
+			});
 	}, []);
 
-
 	function onChangeStatus(id) {
+		console.log(id);
 		setTodos(todosState => todosState.map(e => e._id === id ? { ...e, isComplete: !e.isComplete } : e))
+	}
+
+	function onAddTodoClick() {
+		setIsVisible(state => !state);
+	}
+
+	function onTodoSubmit(e, data) {
+		e.preventDefault();
+
+		fetch('http://localhost:3030/jsonstore/todos',
+			{
+				method: 'POST',
+				body: JSON.stringify({ text: data, isComplete: false })
+			}
+		).then(res => setTodos(state => [...state, { _id: `todo_${todos.length}`, text: data, isComplete: false }]));
+
+		setIsVisible(state => !state);
 	}
 
 	return (
@@ -31,14 +53,13 @@ function App() {
 					<h1>Todo List</h1>
 
 					<div className="add-btn-container">
-						<button className="btn">+ Add new Todo</button>
+						<button className="btn" onClick={onAddTodoClick}>+ Add new Todo</button>
 					</div>
 
+					{isVisible && <AddForm onTodoSubmit={onTodoSubmit} />}
+
 					<div className="table-wrapper">
-
 						{todos ? <Table todos={todos} onChangeStatus={onChangeStatus} /> : <Spinner />}
-
-
 					</div>
 				</section>
 			</main>
--- todo-list/client/src/components/AddForm.js
+++ todo-list/client/src/components/AddForm.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 
 export const AddForm = ({ onTodoSubmit }) => {
+    const [inputText, setInputText] = useState('');
+
+    function onInputChange(e) {
+        setInputText(e);
+    }
 
     return (
-        <form className="form-inline" onSubmit={(e) => onTodoSubmit(e)}>
+        <form className="form-inline" onSubmit={(e) => onTodoSubmit(e, inputText)}>
             <label htmlFor="todo" style={{ fontSize: "2rem", paddingRight: "10px" }}>Todo</label>
-            <input type="text" className="form-control mb-2 mr-sm-2" id="todo" />
+            <input type="text" className="form-control mb-2 mr-sm-2" id="todo" value={inputText} onChange={e => onInputChange(e.target.value)} />
 
             <button type="submit" className="btn btn-primary mb-2">Submit</button>
         </form>
